d92637d1d265fd795a0ae2357a459da272cf7f53
unknown <felix.ing86@gmail.com>
2018-04-03 00:53:26 -0400

    first

---
+++ app/src/main/java/com/example/felix/retrofit2/GetData.java
@@ -0,0 +1,20 @@
+
+package com.example.felix.retrofit2;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+/**
+ * Created by Felix on 3/30/2018.
+ */
+
+public interface GetData {
+
+    String BASE_URL = "https://simplifiedcoding.net/demos/";
+
+    @GET("marvel")
+    Call<List<Hero>> getHeroes();
+}
